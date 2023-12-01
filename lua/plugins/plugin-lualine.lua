f9bedaceb4a3b368676c6bdbf3004def6a2c1cfd
kyo <123>
2023-12-01 16:42:31 +0800

    basic

--- lua/plugins/plugin-lualine.lua
+++ lua/plugins/plugin-lualine.lua
@@ -2,7 +2,11 @@ return {
     {
         'nvim-lualine/lualine.nvim',
         config = function()
-            require('lualine').setup()
+            require('lualine').setup {
+		options = {
+		    theme = 'tokyonight'
+		}
+	    }
         end
     }
 }
